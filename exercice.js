4119f05aa2a4484971ef500684d22f252501b024
mathieu <mr.rousseaumathieu@gmail.com>
2017-01-12 17:45:33 +0100

    Add files via upload

---
+++ exercice.js
@@ -0,0 +1,10 @@
+console.log("Excercice Java");
+
+
+//Hello World.
+
+var hello = "Hello World";
+console.log(hello);
+
+var monTitre = document.querySelector('h1');
+monTitre.textContent = 'Hello world!';
