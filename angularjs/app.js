e4d29c3a4c0831b5f6b3c2f1460d655fdf75e6ce
Mathieu ROUSSEAU <mr.rousseaumathieu@gmail.com>
2017-01-17 11:39:30 +0100

    exercice formulaire angular

---
+++ angularjs/app.js
@@ -0,0 +1,68 @@
+angular.module('app', [])
+
+.run(function() {
+  console.log('PizzaYOLO !');
+})
+
+.controller('MyFirstController',class MyFirstController {
+    constructor() {
+        this.users = [
+            { name: 'un', age: 10 },
+            { name: 'deux', age: 20 },
+            { name: 'trois', age: 33 },
+            { name: 'quatre', age: 42 },
+        ];
+        this.predicat = 'name';
+        this.reverse = false;
+
+    }
+
+    sort(predicat) {
+        if (this.predicat === predicat) {
+            this.reverse = !this.reverse;
+        }
+        this.predicat = predicat;
+    }
+
+    save (form, user) {
+        if (user.age !== ''){
+            console.log('ok')
+        }
+
+
+        let tempo = {};
+
+        tempo.name =  user.name;
+        tempo.age = user.age;
+        
+        this.users.push(tempo);
+
+    }
+})
+
+.filter('exclamation', function () {
+    return (input, size = 1) => {
+        if (typeof input !== 'string') return '';
+        return input + '!'.repeat(size);
+    }
+})
+
+.filter('eldest', function () {
+    return (users) => {
+        if (!users instanceof Array) return '';
+
+        let max = users[0];
+        users.forEach((user) => {
+            if (user.age && user.age > max.age) max = user;
+        });
+
+        return max.name;
+
+        // const eldest = users.reduce((max, user) => {
+        //     if (user.age && user.age > max.age) return user;
+        //     return max;
+        // }, {name: '', age: 0});
+
+        // return eldest.name;
+    }
+})
