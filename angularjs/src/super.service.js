9b759de024643bc94130403bc984ee2710d1bff2
Mathieu ROUSSEAU <mr.rousseaumathieu@gmail.com>
2017-01-17 14:54:00 +0100

    integration d'un regquete + srv json

--- angularjs/src/super.service.js
+++ angularjs/src/super.service.js
@@ -1,15 +1,9 @@
 export class SuperService {
-    constructor($timeout) {
-        this.$timeout = $timeout;
+    constructor($http) {
+        this.$http = $http;
     }
 
     getUsers() {
-        return this.$timeout(2000)
-            .then(() =>  [
-                { name: 'un', age: 10 },
-                { name: 'deux', age: 20 },
-                { name: 'trois', age: 33 },
-                { name: 'quatre', age: 42 },
-            ]);
+        return this.$http({method: 'GET', url: 'http://localhost:3000/users'}).then( respons => { return respons.data});
     }
 }
